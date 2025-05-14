b5bdb2f9676eba1484aaea29cd01e2408c38eb64
pugazhenthi2002 <pugazhenthi2002@gmail.com>
2025-05-14 10:04:47 +0530

    Test Project included

---
+++ TestProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using PowerLibrary;
+using PowerLibrary.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String x = "12";
+            if (x.IsNullOrEmpty())
+            {
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
